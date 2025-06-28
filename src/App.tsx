94722dca4ddf2bf86af73ba84a6dec07f79ca32d
sohan <sohan@aereo.io>
2025-06-28 22:19:33 +0530

    [Feat] Implement map

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
 import { Header } from "./components";
+import maplibregl, { Map } from "maplibre-gl";
+import "maplibre-gl/dist/maplibre-gl.css";
+import { useEffect, useRef } from "react";
 
 const App = () => {
+  const mapContainer = useRef(null);
+
+  useEffect(() => {
+    if (!mapContainer.current) {
+      return;
+    }
+
+    const map = new maplibregl.Map({
+      container: mapContainer.current, // container id
+      style: "https://demotiles.maplibre.org/style.json", // style URL
+      center: [0, 0], // starting position [lng, lat]
+      zoom: 1, // starting zoom
+    });
+  }, []);
+
   return (
     <div>
       <Header />
+      <div className=" w-[20rem] h-[20rem]">
+        <div ref={mapContainer} className=" h-full w-full"></div>
+      </div>
     </div>
   );
 };
