2c0d07ad291ee26cd22b35bc021f583b2315ab84
sohan <sohan96973552@gmail.com>
2025-07-14 19:35:30 +0530

    Change header styles

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,6 +1,8 @@
+import React from "react";
+
 export const Header = () => {
   return (
-    <header className="text-black h-[4rem] p-[1rem]">
+    <header className="text-black h-[4rem] p-[1rem] border-black border-solid border-b">
       <h1 className="text-xl">Map</h1>
     </header>
   );
